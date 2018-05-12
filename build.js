ced38b59aa9647a826138511707d098eef6edbe8
Fergus King <fergus.king@sse.com>
2018-05-12 15:30:55 +0100

    App working with handlebar as templating engine

--- build.js
+++ build.js
@@ -1,33 +1,61 @@
-const metalsminth = require('metalsmith')
+const glob = require('glob')
+const Metalsmith = require('metalsmith')
+const layouts = require('metalsmith-layouts')
 const assets = require('metalsmith-assets')
-const templates = require('metalsmith-templates')
+const sass = require('metalsmith-sass')
 const markdown = require('metalsmith-markdown')
 const dataMarkdown = require('metalsmith-data-markdown')
 const contentful = require('contentful-metalsmith')
+
 require('dotenv').config()
 
-metalsminth(__dirname)
-    .source('src')
-    .destination('build')
-    .use(
-        contentful({
-            space_id: process.env.CMS_SPACE_ID,
-            access_token: process.env.CMS_ACCESS_TOKEN
-        })
-    )
-    .use(
-        templates({
-            engine: 'pug'
-        })
-    )
-    .use(markdown())
-    .use(
-        dataMarkdown({
-            removeAttributeAfterwards: true
-        })
-    )
-    .build(function(err){
-        if (err) throw err
+const handlebars = require('handlebars')
 
-        console.log('build sucessfull')
-    })
+// add custom helpers to handlebars
+// https://github.com/superwolff/metalsmith-layouts/issues/63
+//
+// using the global handlebars instance
+glob.sync('helpers/*.js').forEach((fileName) => {
+  const helper = fileName.split('/').pop().replace('.js', '')
+
+  handlebars.registerHelper(
+    helper,
+    require(`./${fileName}`)
+  )
+})
+
+Metalsmith(__dirname)
+  .source('src')
+  .destination('build')
+  .use(contentful({
+    space_id: process.env.CMS_SPACE_ID,
+    access_token: process.env.CMS_ACCESS_TOKEN,
+    common: {
+      featured_author: {
+        limit: 1,
+        filter: {
+          'sys.id[in]': '5JQ715oDQW68k8EiEuKOk8'
+        }
+      }
+    },
+  }))
+  .use(layouts({
+    engine: 'handlebars',
+    partials: 'partials'
+  }))
+  .use(assets({
+    source: 'assets/',
+    destination: 'assets/'
+  }))
+  .use(sass({
+    outputStyle: 'compressed'
+  }))
+  .use(markdown())
+  .use(dataMarkdown({
+    removeAttributeAfterwards: true
+  }))
+  .build(function (err) {
+    if (err) throw err
+
+    console.log('Successfully build metalsmith')
+  })
