2a554730525832a566f6318675b2c1b463701288
Fergus King <fergus.king@sse.com>
2018-05-12 15:44:00 +0100

    clean handlebars version, just about to try and convert it to pug

--- build.js
+++ build.js
@@ -1,28 +1,11 @@
-const glob = require('glob')
 const Metalsmith = require('metalsmith')
 const layouts = require('metalsmith-layouts')
 const assets = require('metalsmith-assets')
-const sass = require('metalsmith-sass')
 const markdown = require('metalsmith-markdown')
 const dataMarkdown = require('metalsmith-data-markdown')
 const contentful = require('contentful-metalsmith')
 
 require('dotenv').config()
-
-const handlebars = require('handlebars')
-
-// add custom helpers to handlebars
-// https://github.com/superwolff/metalsmith-layouts/issues/63
-//
-// using the global handlebars instance
-glob.sync('helpers/*.js').forEach((fileName) => {
-  const helper = fileName.split('/').pop().replace('.js', '')
-
-  handlebars.registerHelper(
-    helper,
-    require(`./${fileName}`)
-  )
-})
 
 Metalsmith(__dirname)
   .source('src')
@@ -40,15 +23,11 @@ Metalsmith(__dirname)
     },
   }))
   .use(layouts({
-    engine: 'handlebars',
-    partials: 'partials'
+    engine: 'handlebars'
   }))
   .use(assets({
     source: 'assets/',
     destination: 'assets/'
-  }))
-  .use(sass({
-    outputStyle: 'compressed'
   }))
   .use(markdown())
   .use(dataMarkdown({
